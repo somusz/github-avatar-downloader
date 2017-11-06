13a72f7f3d52634b4a4eb0ebd71638f3290c010f
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-06 23:28:38 +0000

    style changes for final submission

--- download_avatars.js
+++ download_avatars.js
@@ -1,35 +1,48 @@
+//download_avatars.js
+//downloading avatars from accessible Github project contributors, ...
+//...provided valid repo owner and repo name are entered through command line
+
+//requiring external resources: request, fs and github token through external file
 var request = require('request');
 var fs = require('fs');
 var secrets = require('./secrets');
 
+//input validity check - return error message in case of incomplete input
 if (process.argv[2] && process.argv[3]) {
-var owner = process.argv[2];
-var repo = process.argv[3];
+  var owner = process.argv[2];
+  var repo = process.argv[3];
 } else {
-  console.log('Please provide a valid Github repo owner id and a valid repo name.');
-  return;
-}
+    console.log('Please provide a valid Github repo owner id and a valid repo name.');
+    return;
+  }
 
+//this function executes given action (callback) on provided repo info
 function getRepoContributors(repoOwner, repoName, cb) {
-   var options = {
-     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-     headers: {
+//the function builds url and request info
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
       'User-Agent': 'request',
       'Authorization': secrets.GITHUB_TOKEN
-     }
-   };
-   request(options, (err, res, body) => {
+    }
+  };
+//the function requests response from the url
+  request(options, (err, res, body) => {
     if (err) {
       console.log("Errors:", err);
     };
+//the function transforms returned response into JSON data
     var jsonData = JSON.parse(body);
+//the function iterates over items in data
     jsonData.forEach(function(contributor) {
       var theFilePath = 'avatars/' + contributor.login + ".jpg";
+//the function executes given callback function, on valid repo data
       cb(contributor.avatar_url,theFilePath);
     })
-   });
+  });
 }
 
+//this function specifies a stream to get the data from the url and write it to given file
 function downloadImageByURL(url, filePath) {
   request.get(url)
          .on('error', function(err) {
@@ -41,5 +54,6 @@ function downloadImageByURL(url, filePath) {
          .pipe(fs.createWriteStream(filePath));
 }
 
+//calling function getRepoContributors on valid repo info and given action
 getRepoContributors(owner, repo, downloadImageByURL);
 
