9cf6cbdb4bce40ef8b5e5685848be61b08f9a8d4
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-06 22:43:26 +0000

    downloader complete

--- download_avatars.js
+++ download_avatars.js
@@ -16,12 +16,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
    });
 }
 
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log("Errors:", err);
-//   result.forEach(function(contributor) {
-//     console.log(contributor.avatar_url);
-//   });
-// });
+getRepoContributors("jquery", "jquery", function(err, result) {
+  if (err) { console.log("Errors:", err); }
+  result.forEach(function(contributor) {
+    var theFilePath = 'avatars/' + contributor.login + ".jpg";
+    downloadImageByURL(contributor.avatar_url,theFilePath);
+  });
+});
 
 
 function downloadImageByURL(url, filePath) {
@@ -29,10 +30,9 @@ function downloadImageByURL(url, filePath) {
          .on('error', function(err) {
           throw err;
          })
-         .on('response', function () {
-          console.log('stream is on');
+         .on('end', function () {
+          console.log(filePath + ' downloaded');
          })
          .pipe(fs.createWriteStream(filePath));
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani2.jpg")
