ed75e98a73b1f24202d68feec0f785472ab17454
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-07 02:13:01 +0000

    advanced error handling

--- download_avatars.js
+++ download_avatars.js
@@ -8,17 +8,33 @@ var request = require('request');
 var fs = require('fs');
 var secrets = require('./secrets');
 
+//validating input info
+fs.open('process.env','r', function (err) {
+  if (err) {
+    throw new Error('Authorization is unsuccessful due to missing credentials.');
+  } else if (!process.env.GITHUB_TOKEN) {
+    throw new Error('Authorization is unsuccessful due to incomplete credentials.');
+  }
+});
+
+
 //input validity check - return error message in case of incomplete input
 if (process.argv[2] && process.argv[3]) {
   var owner = process.argv[2];
   var repo = process.argv[3];
 } else {
-    console.log('Please provide a valid Github repo owner id and a valid repo name.');
+    throw new Error('Please provide a valid Github repo owner id and a valid repo name.');
     return;
   }
 
 //this function executes given action (callback) on provided repo info
 function getRepoContributors(repoOwner, repoName, cb) {
+//checking destination folder and creating if does not yet exist
+  fs.open('./avatars','r', function(err) {
+    if (err) {
+      fs.mkdir('./avatars');
+    }
+  });
 //the function builds url and request info
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -28,27 +44,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 //the function requests response from the url
-  request(options, (err, res, body) => {
-    if (err) {
-      console.log("Errors:", err);
-    };
+  request(options, function(err, res, body) {
 //the function transforms returned response into JSON data
-    var jsonData = JSON.parse(body);
+    if (res.statusCode > 299) {
+      return console.log('This repo owner - repo name combo doesn\'t exist.');
+    } else {
+        var jsonData = JSON.parse(body);
 //the function iterates over items in data
-    jsonData.forEach(function(contributor) {
-      var theFilePath = 'avatars/' + contributor.login + ".jpg";
+        jsonData.forEach(function(contributor) {
+          var theFilePath = 'avatars/' + contributor.login + ".jpg";
 //the function executes given callback function, on valid repo data
-      cb(contributor.avatar_url,theFilePath);
-    })
+          cb(contributor.avatar_url,theFilePath);
+        });
+    }
   });
 }
 
 //this function specifies a stream to get the data from the url and write it to given file
 function downloadImageByURL(url, filePath) {
   request.get(url)
-         .on('error', function(err) {
-          throw err;
-         })
          .on('end', function () {
           console.log(filePath + ' downloaded');
          })
